3094b0baae869a01e376070db4e9a1fa8dbe5388
Grindelward <przemyslaw.nowicki93@gmail.com>
2017-01-10 21:06:52 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<form action="action_page.php" method="GET">
+  Product ID:<br>
+  <input type="text" name="IDproduktu" value="">
+  <br>
+  <br>
+  <input type="submit" value="Submit">
+</form> 
+ 
+
+
+
+
+
+
+</body>
+</html>
